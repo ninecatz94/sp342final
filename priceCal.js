7dfd57d555011398c62a50486e280b4cfb4d15ee
Yothin <2010511101002@live4.utcc.ac.th>
2022-04-27 14:44:56 +0700

    adding funtion cal price

--- priceCal.js
+++ priceCal.js
@@ -2,8 +2,23 @@ function priceCalculate() {
   let ticknum = (document.getElementById("ticknum").value).trim();
   let selectObj = document.getElementById("event");
   let eventId = parseInt(selectObj.options[selectObj.selectedIndex].value);
-  
-  return 0;
+  if (eventId == 1){
+	  evenprice = 91.25 ;
+	  total = evenprice * ticknum;
+  }
+  if (eventId == 2){
+	  evenprice = 79.88 ;
+	  total = evenprice * ticknum;
+  }
+  if (eventId == 3){
+	  evenprice = 83.12 ;
+	  total = evenprice * ticknum;
+  }
+  else{
+	  evenprice = 100 ;
+	  total = evenprice * ticknum;
+  }
+  return total;
 }
 
 
